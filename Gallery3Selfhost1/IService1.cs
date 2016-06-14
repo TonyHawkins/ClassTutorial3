c0a153987c419e9dd4a7f980bdd4c26455d018d6
Tony Hawkins <Tony-Hawkins@live.nmit.ac.nz>
2016-06-14 12:02:12 +1200

    Service Running

---
+++ Gallery3Selfhost1/IService1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery3Selfhost1
+{
+    [ServiceContract]
+    interface IService1
+    {
+        [OperationContract]
+        List<string> GetArtistNames();
+    }
+}
---
+++ Gallery3Selfhost1/Service1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Gallery3Selfhost1
+{
+    class Service1 : IService1
+    {
+        public List<string> GetArtistNames()
+        {
+            using (Gallery_DataEntities lcContext = new Gallery_DataEntities())
+                return lcContext.Artists
+                        .Select(lcArtist => lcArtist.Name)
+                        .ToList();
+        }
+    }
+}
